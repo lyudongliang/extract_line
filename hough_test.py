713b59b1b3508fea89e5addc88c4afd4590618d7
lyu <dongliang040@sina.com>
2019-03-27 01:17:59 +0800

    add file to extract line

---
+++ hough_test.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+import time
+import os
+
+
+# type = 'home'
+# type = 'table'
+# type = 'window'
+type = 'office'
+
+picture_file = type + '_corner.jpg'
+picture_directory = 'D:\data\picture'
+
+src_picture = os.path.join(picture_directory, picture_file)
+
+img = cv2.imread(src_picture)
+
+time0 = time.time()
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+gray = cv2.GaussianBlur(gray, (3, 3), 0)
+edges = cv2.Canny(gray, 30, 100, apertureSize=3)
+
+edge_file = type + '_corner_edge.jpg'
+
+cv2.imwrite(os.path.join('picture', edge_file), edges)
+
+lines = cv2.HoughLinesP(edges, 1, 0.5 * np.pi/180, 200, minLineLength=20, maxLineGap=50)
+
+print('time', time.time() - time0)
+
+for line in lines:
+    x1, y1, x2, y2 = line[0]
+    cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+output_dirctory = os.path.join('picture', picture_file)
+cv2.imwrite(output_dirctory, img)
+
+print('finish')
+
+
