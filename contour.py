48c875c062fbaf1235848a3028a13742bcb4d928
lyu <dongliang040@sina.com>
2019-03-31 23:21:02 +0800

    use contours info

--- contour.py
+++ contour.py
@@ -3,11 +3,18 @@ import numpy as np
 import os
 
 
+def get_sorted_contours(contours):
+    sorted_contours = sorted(contours, key=lambda x: -x.shape[0])
+
+    return sorted_contours
+
+
 def get_contours(image):
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    _, binary_image = cv.threshold(gray, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
-    # cv.imshow('binary_image', binary_image)
-    # cv.waitKey(0)
+    _, binary_image = cv.threshold(gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    # binary_image = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+    cv.imshow('binary_image', binary_image)
+    cv.waitKey(0)
 
     contours, hierarchy = cv.findContours(binary_image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
@@ -20,6 +27,15 @@ def get_contours(image):
     new_contours = sorted_contours[:10]
     cv.drawContours(image, new_contours, -1, (255, 0, 0), 2)
 
+    # x, y, w, h = cv.boundingRect(new_contours[0])
+    # image = cv.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    for i in range(10):
+        rect = cv.minAreaRect(new_contours[i])
+        box = cv.boxPoints(rect)
+        box = np.int0(box)
+        image = cv.drawContours(image, [box], 0, (0, 0, 255), 2)
+
     cv.imshow('walk_corner_contours', image)
     cv.waitKey(0)
     cv.destroyAllWindows()
@@ -27,14 +43,8 @@ def get_contours(image):
     return image
 
 
-def get_sorted_contours(contours):
-    sorted_contours = sorted(contours, key=lambda x: -x.shape[0])
-
-    return sorted_contours
-
-
 picture_types = ['walk', 'road', 'niu', 'home', 'table', 'window', 'office']
-type = picture_types[0]
+type = picture_types[4]
 
 picture_file = type + '_corner.jpg'
 picture_directory = 'D:\data\picture'
@@ -43,6 +53,6 @@ src_image = cv.imread(os.path.join(picture_directory, picture_file))
 
 image_contours = get_contours(src_image)
 
-output_dirctory = os.path.join('picture', type + '_contours.jpg')
+output_directory = os.path.join('picture', type + '_contours.jpg')
 
-cv.imwrite(output_dirctory, image_contours)
+cv.imwrite(output_directory, image_contours)
